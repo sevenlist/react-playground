c473d76567826f5c226d85c27c10feaaa8f8d157
sevenlist <sevenlist@web.de>
2016-03-25 21:51:14 +0100

    Formatting

--- app/App.js
+++ app/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
 
 class App extends Component {
-  render(){
-    return (
-	  <h1>Hello World</h1>
-    );
-  }
+    render() {
+        return (
+            <h1>Hello, World!</h1>
+        );
+    }
 }
 
 React.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-	entry: __dirname + "/app/App.js",
-	output: {
-		path: __dirname,
-		filename: "bundle.js"
-	},
-	module: {
-	  loaders: [{
-	      test: /\.jsx?$/,
-	      exclude: /node_modules/,
-	      loader: 'babel',
-	      query: {
-	        presets: ['react', 'es2015']
-	      }
-	  }]
-	}
+    entry: __dirname + "/app/App.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    }
 };
